9f63a10ff94fd8a5a8ee093c9dae0ffc1caee49f
Gisel Cupul <Gisel Cupul@DESKTOP-9U13DQB>
2020-11-11 02:27:38 -0600

    style-changes

---
+++ IMASD/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace IMASD
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryscroll").Include(
+                        "~/Scripts/jquery.scrollbar.min.js",
+                        "~/Scripts/jquery-scrollLock.min.js"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/argon").Include(
+                      "~/Scripts/argon.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/cookie").Include(
+                      "~/Scripts/js.cookie.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/nucleo.css",
+                      "~/Content/argon.css"));
+        }
+    }
+}
